e555a13d28dcae9980dc99d266142303dea8c04a
kindlingscript <inflamedmuse@gmail.com>
2016-08-15 00:09:17 -0500

    Added strikethrough functionality

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,7 +4,8 @@ $(function() {
   // using <li> tags
   function taskHTML(task) {
     var checkedStatus = task.done ? "checked" : "";
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox" data-id="' + task.id + '"' + checkedStatus + '><label>' + task.title + '</label></div></li>';
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id + '" class="'+ liClass + '"><div class="view"><input class="toggle" type="checkbox" data-id="' + task.id + '"' + checkedStatus + '><label>' + task.title + '</label></div></li>';
 
     return liElement;
   }
@@ -24,6 +25,11 @@ $(function() {
       task: {
         done: doneValue
       }
+    }).success(function(data) {
+      var liHTML = taskHTML(data);
+      var $li = $('#listItem-' + data.id);
+      $li.replaceWith(liHTML);
+      $('.toggle').change(toggleTask);
     });
   }
 
